7db56e216e18d29472dc2b463908161d29808e6c
wang_ju <wang_ju@arcplus.com.cn>
2022-06-23 10:03:16 +0800

    fix: stronger randx

--- randx/randx.go
+++ randx/randx.go
@@ -7,14 +7,24 @@ import (
 )
 
 // NewString 生成随机字符串
-func NewString(n int) string {
+func NewString(n int, seed ...int64) string {
 	if n > 1024 {
 		return "length over 1024"
 	}
 	source := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	l := len(source)
 
-	rand.Seed(time.Now().Unix())
+	if len(seed) > 0 {
+		var count int64
+		for _, v := range seed {
+			count += v
+		}
+
+		rand.Seed(count)
+	} else {
+		rand.Seed(time.Now().Unix())
+	}
+
 	buf := make([]byte, n)
 	for i := 0; i < n; i++ {
 		buf[i] = source[rand.Intn(l)]
@@ -23,7 +33,17 @@ func NewString(n int) string {
 	return *(*string)(unsafe.Pointer(&buf))
 }
 
-func NewInt(n int) int {
+func NewInt(n int, seed ...int64) int {
+	if len(seed) > 0 {
+		var count int64
+		for _, v := range seed {
+			count += v
+		}
+
+		r := rand.New(rand.NewSource(count))
+		return r.Intn(n)
+	}
+
 	r := rand.New(rand.NewSource(time.Now().Unix()))
 	return r.Intn(n)
 }
