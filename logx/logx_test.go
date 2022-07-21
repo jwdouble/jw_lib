d5f8084479aa5fa09ed994e9c91eab996672c7e5
wang_ju <wang_ju@arcplus.com.cn>
2022-07-21 15:33:32 +0800

    stash

--- logx/logx_test.go
+++ logx/logx_test.go
@@ -1,25 +1,48 @@
 package logx
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 
-	"jw.lib/rdx"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"jw.lib/timex"
 )
 
-func Test_log(t *testing.T) {
-	Info("this is %s code, no.%d", []any{"jw", 1})
+func Test_zerolog(t *testing.T) {
+	zerolog.TimeFieldFormat = timex.DateTimeFormat
+
+	log.Print("hello world")
+	log.Info().Msg("hello world")
+
+	log.Debug().
+		Str("Scale", "833 cents").
+		Float64("Interval", 833.09).
+		Msg("Fibonacci is everywhere")
+
+	log.Debug().
+		Str("Name", "Tom").
+		Send() // Send is equivalent to calling Msg("")
+
+	err := errors.New("seems we have an error here")
+	log.Error().Err(err).Msg("haha")
+}
+
+func Test_mylog(t *testing.T) {
+	err := errors.New("this is error")
+	Errorf(err, "this is error %s", "test")
 }
 
 func Test_redis(t *testing.T) {
-	Info("INFO TEST")
-
-	sc := rdx.GetRdxOperator().RPop("logx")
-	r, err := sc.Result()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(r)
+	//Info("INFO TEST")
+	//
+	//sc := rdx.GetRdxOperator().RPop("logx")
+	//r, err := sc.Result()
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//fmt.Println(r)
 }
